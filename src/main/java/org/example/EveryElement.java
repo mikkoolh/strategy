f75fb6c95516c4a22eb5f550df4a694c8601e204
Mikko Hänninen <mikkoolh@metropolia.fi>
2023-10-24 17:45:13 +0300

    valmis

--- src/main/java/org/example/EveryElement.java
+++ src/main/java/org/example/EveryElement.java
@@ -1,13 +1,15 @@
 package org.example;
 
+import java.util.Iterator;
 import java.util.List;
 
 public class EveryElement implements ListConverter{
     @Override
     public String listToString(List<Object> list) {
         StringBuilder string = new StringBuilder();
-        for (Object item : list) {
-            string.append(item.toString()).append("\n");
+        Iterator<Object> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            string.append(iterator.next().toString()).append("\n");
         }
         return string.toString();
     }
--- src/main/java/org/example/EverySecondElement.java
+++ src/main/java/org/example/EverySecondElement.java
@@ -6,8 +6,9 @@ public class EverySecondElement implements ListConverter{
     @Override
     public String listToString(List<Object> list) {
         StringBuilder string = new StringBuilder();
-        for (int i = 0; i < list.size(); i++) {
-            string.append(list.get(i).toString());
+        Object[] array = list.toArray();
+        for (int i = 0; i < array.length; i++) {
+            string.append(array[i].toString());
             if (i % 2 == 1) {
                 string.append("\n");
             } else {
